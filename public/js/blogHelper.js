b1a8ecaad0d961c0acd2df3db6430b8e7b03e939
jose Diaz <jrd.palacios@gmail.com>
2015-03-10 21:17:46 -0400

    Added UI programatic creation of parragraph & images input boxes

--- public/js/blogHelper.js
+++ public/js/blogHelper.js
@@ -8,11 +8,41 @@
 
 
 $(document).ready(function(){
-    $("#addParr").click(function(){
-        $("#body-content").after("<p>Jquery FTW</p>");
+		var pCount = 0;
+		var iCount = 0;
+
+		// Dynamic parragraphs
+    $('#addParr').click(function(){
+        $('#article-body-content').append('<label id="labelParr'+pCount+'" for="parragraph'+pCount+'">parragraph'+pCount+'</label><textarea rows="4" cols="100" id="parragraph'+pCount+'" ></textarea>');
+    		pCount++;
+    		console.log('incrementing pCount to: ' + pCount);
     });
 
-    $("#addImg").click(function(){
-        $("#body-content").after("<p>Jquery FTWimg</p>");
+    $('#addImg').click(function(){
+        $('#article-body-content').append('<label id="labelImg'+iCount+'" for="img-'+iCount+'">Image URL#'+iCount+': </label><input type="text" id="img-'+iCount+'">');
+        iCount++;
+        console.log('incrementing iCount to: ' + iCount);
     });
+
+    // Removing last dynamically added parragraphs
+    $('#remParr').click(function(){
+    		if(pCount != 0)
+	    	{	
+	    		pCount--;
+	    		console.log('decrementing pCount to: ' + pCount);
+	    		$('#labelParr'+pCount).remove();
+	        $('#parragraph'+pCount).remove();
+    		}
+    });
+
+     $("#remImg").click(function(){
+     		if(iCount != 0)
+     		{	
+	     		iCount--;
+	     		console.log('decrementing iCount to: ' + iCount);
+	     		$('#labelImg'+iCount).remove();
+	        $('#img-'+iCount).remove();
+    		}
+    });
+
 });
